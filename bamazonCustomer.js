4460c50410638c78b1e15330e3489a29b9142378
nevermindthelabel <nevermindthelabel@gmail.com>
2018-12-28 21:30:04 -0700

    wip added code for getting product id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,49 +4,61 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 var databasePassword = process.env.DATABASE_PASSWORD;
 var connection = mysql.createConnection({
-    host: 'localhost',
-    // port: 3306,
-    user: 'root',
-    password: databasePassword,
-    database: 'bamazon'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: databasePassword,
+  database: 'bamazon'
 });
 
 connection.connect(function (error) {
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+    console.log(error)
+  }
 });
-    start();
+start();
 
 function start() {
 
-    console.log('Welcome to my store. Here are the products available for purchase -');
-    connection.query('SELECT * From products', function(error, results) {
-        if (error){
-            console.log(error);
-        }
-        for (var i = 0; i < results.length; i++)
-        console.log(`
+  console.log('Welcome to my store. Here are the products available for purchase -');
+  connection.query('SELECT item_id, product_name, price From products', function (error, results) {
+    if (error) {
+      console.log(error);
+    }
+    for (var i = 0; i < results.length; i++)
+      console.log(`
         Item ID: ${results[i].item_id}
         Name: ${results[i].product_name}
         Cost: $${results[i].price}
         `)
-        buyId();
-    })
-}
-function buyId() {
-    inquirer
-        .prompt({
+    buyId();
+    function buyId() {
+
+      inquirer
+        .prompt([
+          {
             name: 'productId',
             type: 'input',
-            message: 'Which item would you like to buy by id number?'
-        }).then(function(answer){
-            var query = 'SELECT item_id FROM products WHERE ?';
-            connection.query(query, { productId: answer.productId }, function (error, response) {
-                for (var i = 0; i < response.length; i++) {
-                    console.log('id :' + response[i])
-                }
-            })
+            message: 'Enter the id number of the item you\'d like to purchase.',
+            validate: function (idNumber) {
+              if (isNaN(idNumber) == false && parseInt(idNumber) <= results.length && parseInt(idNumber) > 0) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }
+        ]).then(function (answer) {
+          // console.log(answer)
+          var userInput = parseInt(answer.productId) 
+          var query = 'SELECT item_id FROM products WHERE item_id='+ userInput  ;
+          connection.query(query, { productId: answer.productId }, function (error, response) {
+            if (error) throw error;
+            console.log(response);
+          })
         })
+    }
+  })
 }
-connection.end();
+
+// connection.end();
