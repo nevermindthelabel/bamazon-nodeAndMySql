4f13c23d7bab1e395d413938a9c20fe57881882b
nevermindthelabel <nevermindthelabel@gmail.com>
2018-12-29 13:58:46 -0700

    completed basic tasks for bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,64 +1,84 @@
 require('dotenv').config();
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-var fs = require('fs');
-var databasePassword = process.env.DATABASE_PASSWORD;
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const databasePassword = process.env.DATABASE_PASSWORD;
+const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: databasePassword,
-  database: 'bamazon'
+  database: 'bamazon',
 });
 
-connection.connect(function (error) {
+connection.connect((error) => {
   if (error) {
-    console.log(error)
+    console.log(error);
+    return;
   }
+  start();
 });
-start();
 
 function start() {
-
   console.log('Welcome to my store. Here are the products available for purchase -');
-  connection.query('SELECT item_id, product_name, price From products', function (error, results) {
+  connection.query('SELECT item_id, product_name, price From products', (error, results) => {
     if (error) {
       console.log(error);
     }
-    for (var i = 0; i < results.length; i++)
+    for (let i = 0; i < results.length; i++) {
       console.log(`
         Item ID: ${results[i].item_id}
         Name: ${results[i].product_name}
         Cost: $${results[i].price}
-        `)
+        `);
+    }
     buyId();
     function buyId() {
-
       inquirer
         .prompt([
           {
             name: 'productId',
             type: 'input',
             message: 'Enter the id number of the item you\'d like to purchase.',
-            validate: function (idNumber) {
-              if (isNaN(idNumber) == false && parseInt(idNumber) <= results.length && parseInt(idNumber) > 0) {
+            validate(idNumber) {
+              if (isNaN(idNumber) === false && parseInt(idNumber) <= results.length && parseInt(idNumber) > 0) {
                 return true;
-              } else {
-                return false;
               }
-            }
-          }
-        ]).then(function (answer) {
-          // console.log(answer)
-          var userInput = parseInt(answer.productId) 
-          var query = 'SELECT item_id FROM products WHERE item_id='+ userInput  ;
-          connection.query(query, { productId: answer.productId }, function (error, response) {
+              return false;
+            },
+          },
+          {
+            name: 'numProducts',
+            type: 'input',
+            message: 'Enter the number of products you\'d like to buy.',
+            validate(idNumber) {
+              if (isNaN(idNumber) === false && parseInt(idNumber) <= results.length && parseInt(idNumber) > 0) {
+                return true;
+              }
+              return false;
+            },
+          },
+        ]).then((answer) => {
+          const userInput = parseInt(answer.productId);
+          const query = `SELECT * FROM products WHERE item_id = ${userInput}`;
+          connection.query(query, (error, response) => {
             if (error) throw error;
-            console.log(response);
-          })
-        })
-    }
-  })
-}
+            const numInStock = parseInt(response[0].stock_quantity);
+            const numUserRequested = parseInt(answer.numProducts);
+            const cost = parseFloat(response[0].price);
+            const arizonaSalesTax = parseFloat(1.086);
+            const totalCost = (cost * numUserRequested) * arizonaSalesTax;
+            if (numInStock < numUserRequested) {
+              console.log('Insufficient quantity!');
 
-// connection.end();
+              connection.end();
+            } else {
+              connection.query(`UPDATE products SET stock_quantity = stock_quantity - ${numUserRequested} WHERE item_id = ${userInput}`);
+              console.log(`Thank you for your purchase. Your total cost is $${totalCost.toFixed(2)}`);
+              connection.end();
+            }
+          });
+        });
+    }
+  });
+}
