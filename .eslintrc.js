47733a58b70cfd9705ca5a49b4b882d0d4cbeba3
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-04 21:48:59 -0700

    updated rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
+        
         "no-console": 0,
         "no-use-before-define": 0,
         "no-plusplus": 0,
+    },
+    "env": {
+
+        "browser": false,           
+        "node": true
     }
-};
+}
