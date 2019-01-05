37d7f83f1c672bb71b3bdc7afffc3c0efab4f0f1
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-04 21:52:04 -0700

    added departments

--- bamazon.sql
+++ bamazon.sql
@@ -4,14 +4,26 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
-CREATE TABLE products
-(
+CREATE TABLE products (
+
     item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR
 (35) NOT NULL,
     department_name VARCHAR
 (25) NOT NULL,
     price DECIMAL
-(6,2) NULL,
-    stock_quantity INT NULL
+(6,2) DEFAULT NULL,
+    stock_quantity INT DEFAULT NULL,
+    product_sales DECIMAL
+(11, 2) DEFAULT 0
+
 );
+
+CREATE TABLE departments
+(
+  department_id INT PRIMARY KEY NOT NULL
+  AUTO_INCREMENT,
+    department_name VARCHAR
+  (25) NOT NULL,
+    over_head_costs INT NOT NULL
+)
