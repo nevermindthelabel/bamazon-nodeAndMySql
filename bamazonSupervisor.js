6c845dec0a7f255db0ba885a82bd25d5fbaeeef8
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-05 21:46:20 -0700

    wip

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -50,19 +50,26 @@ function viewSalesByDepartment() {
       'total_profit'],
     colWidths: [15],
   });
-  connection.query('SELECT departments.department_id,products.department_name, products.over_head_costs, products.product_sales FROM departments INNER JOIN products ON departments.department_name=departments.department_name', (error, results) => {
+  connection.query(`SELECT 
+  departments.department_id,
+  products.department_name,
+  departments.over_head_costs,
+  products.product_sales, (SUM(product_sales) - SUM(over_head_costs)) AS total_profit
+  FROM departments right JOIN products ON products.department_name = departments.department_name where product_sales > 0`, (error, results) => {
     if (error) {
       console.log(error);
     } for (let i = 0; i < results.length; i++) {
       console.log(`
       ${results[i].department_id},
       ${results[i].department_name},
-      ${results[i].over_head_costs}
+      ${results[i].over_head_costs},
+      ${results[i].product_sales}
       `);
       const id = results[i].department_id;
       const name = results[i].department_name;
       const cost = results[i].over_head_costs;
-      table.push([id, name, cost]);
+      const sales = results[i].product_sales;
+      table.push([id, name, cost, sales]);
       console.log(table.toString());
     }
   });
@@ -88,9 +95,16 @@ function createNewDepartment() {
           'medical',
         ],
       },
+      {
+        name: 'overHeadCost',
+        type: 'input',
+        message: 'What is the overhead in the new department?',
+      },
     ]).then((answer) => {
-      const userAddedDepartment = answer.newDepartmentName;
-      connection.query(`INSERT INTO departments (department_name, over_head_costs) VALUES (${userAddedDepartment, 0})`);
+      const userAddedDepartment = answer.newDepartmentName.replace(' ', '_');
+      const userAddedOverHead = parseInt(answer.overHeadCost);
+      console.log(userAddedDepartment);
+      connection.query(`INSERT INTO departments (department_name, over_head_costs) VALUES (${userAddedDepartment}, ${userAddedOverHead})`);
       connection.end();
     });
 }
